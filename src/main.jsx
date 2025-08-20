0573b181d2c127877da203e0e8efa34b7379b840
Saeeda <saeeda123@gmail.com>
2025-08-20 20:10:43 +0500

    Normalize line endings and add .gitattributes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import favicon from './assets/favicon.png';
+
+const link = document.createElement('link');
+link.rel = 'icon';
+link.type = 'image/png';
+link.href = favicon;
+document.head.appendChild(link);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
